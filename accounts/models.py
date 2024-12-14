257ead07ce5d03e7e1d79bffac13c3863521d10a
Nirbhaya Shrestha <nirbhayashrestha77@gmail.com>
2024-12-14 22:42:02 +0545

    Initial Commit

---
+++ accounts/models.py
@@ -0,0 +1,24 @@
+from datetime import date
+
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser( AbstractUser ):
+	ROLE_CHOICES = (
+		('admin', 'Admin'),
+		('client', 'Client'),
+		('customer', 'Customer'),
+	)
+
+	role = models.CharField( max_length = 10, default = 'customer' )
+
+	email = models.EmailField( unique = True )
+	phone_number = models.CharField( max_length = 15, blank = True,
+	                                 null = True
+	                                 )
+	dob = models.DateField( blank = False, default = date( 1111, 1, 1 ) )
+	address = models.TextField( blank = True, null = True, max_length = 50,
+	                            default = ""
+	                            )
+	password = models.CharField( max_length = 255 )
---
+++ accounts/views.py
@@ -0,0 +1,97 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from django.utils.datastructures import MultiValueDict, MultiValueDictKeyError
+
+from .forms import LoginForm, UserSignUpForm, ClientSignUpForm
+
+
+def login_view( request ):
+	msg = None
+	form = LoginForm( request.POST or None )
+
+	if request.method == "POST":
+		if form.is_valid():
+			username = form.cleaned_data.get( "username" )
+			password = form.cleaned_data.get( "password" )
+
+			user = authenticate( username = username, password = password )
+
+			if user is not None:
+				login( request, user )
+
+				return redirect( "/" )
+			msg = "Invalid credentials"
+		else:
+			msg = "Error validating the form"
+
+	return render( request, "login.html", { "form": form, "msg": msg } )
+
+
+def register_user( request ):
+	msg = None
+	success = False
+
+	if request.method == "POST":
+		if request.user.is_authenticated:
+			return redirect( "/" )
+
+		form = UserSignUpForm( request.POST )
+
+		if form.is_valid():
+			user = form.save(commit = False)
+			user.role = "customer"
+			user.save()
+
+			msg = "User created successfully."
+			success = True
+
+			return redirect( "login" )
+
+		msg = "Form is not valid"
+	else:
+		form = UserSignUpForm()
+
+	return render(
+		request,
+		"register.html",
+		{ "form": form, "msg": msg, "success": success },
+	)
+
+
+def register_hotel_owner( request ):
+	msg = None
+	success = False
+
+	if request.method == "POST":
+		if request.user.is_authenticated:
+			return redirect( "/" )
+
+		form = ClientSignUpForm( request.POST )
+		print( form.errors )
+
+		if form.is_valid():
+			user = form.save( commit = False )
+			user.role = "client"
+			user.save()
+
+			msg = "User created successfully."
+			success = True
+
+			return redirect( "login" )
+
+		msg = "Form is not valid"
+	else:
+		form = ClientSignUpForm()
+
+	return render(
+		request,
+		"client/register.html",
+		{ "form": form, "msg": msg, "success": success },
+	)
+
+
+@login_required()
+def logout_user( request ):
+	logout( request )
+	return redirect( "/" )
